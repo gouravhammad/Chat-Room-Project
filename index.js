5bd04e6dd4e1b22dba451ea8af378981fc79f45a
Gourav Hammad <gouravhammad477@gmail.com>
2019-11-08 23:16:36 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '/public')
+
+app.use(express.static(publicDirectoryPath))
+
+// View Engine Setup
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            
+            message = filter.clean(message)
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
+})
+
+app.get('/',function(req,res){
+    res.render('Home')
+})
+
+app.get('/guide',function(req,res){
+    res.render('Guide')
+})
+
+app.get('/about',function(req,res){
+    res.render('About')
+})
+
+app.get('/chat',function(req,res){
+    res.render('Chat')
+})
+
+app.get('/*',function(req,res){
+    res.render('404')
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
